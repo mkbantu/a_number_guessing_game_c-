40d279924e17711afaea39a67dbfdd60337bd842
Mukundabantu Mohammed <mkbantu56@gmail.com>
2026-04-06 22:25:50 +0200

    ending when you choose

--- Number-guessing-game_C++/Number-guessing-game_C++.cpp
+++ Number-guessing-game_C++/Number-guessing-game_C++.cpp
@@ -8,37 +8,56 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
-	int to_be_guessed= rand() % 100 + 1;
-	int number;
-	int attempts = 5;
-	
-	for (int i = 0; i <= attempts;i++) {
-		cout << "this is the guessing A Number game where you have to guess a number between 1 and 100 in " << attempts << " attempts" << endl;
-		cout << "Enter your guess: ";
-		cin >> number;
-		if (to_be_guessed == number) {
-			cout << "Congratulations! You guessed the number!" << endl;
-			break;
-		}
-		else if(to_be_guessed<number){
-			cout << "The number is too high than your guess. Try again." << endl;
+	while (true) {
+		cout << "=========================THE NUMBER GUESSING GAME========================" << endl;
+
+
+		int attempts = 5;
+
+		for (int i = 1; i <= attempts;i++) {
+			int number;
+			int to_be_guessed = rand() % 100 + 1;
+			cout << "!!!!!This is the guessing A Number game where you have to guess a number between 1 and 100 in " << attempts << " attempts!!!!" << endl;
+			cout << "Enter your guess: ";
+			cin >> number;
+			if (to_be_guessed == number) {
+				cout << "Congratulations! You guessed the number!" << endl;
+				break;
+			}
+			else if (to_be_guessed < number) {
+				cout << "The number is too high than your guess. Try again." << endl;
 
 			}
-		else {
-			cout << "The number is too low than your guess. Try again." << endl;
+			else {
+				cout << "The number is too low than your guess. Try again." << endl;
+			}
+
+
+
+
+
+
+
 		}
+		cout << "================Do you want to play again? (y/n):==================== "<<endl;
+		cout << "1. Yes" << endl;
+		cout << "2. No" << endl;
+		int choice;
+		cin >> choice;
+		switch(choice)
+		{
+		case 1:
 
+			break;
 
-
-
-
-
-
+		case 2:
+			cout << "Thank you for playing! Goodbye!" << endl;
+			return 0;
+		default:
+			break;
+		}
 	}
-	return 0;
 }
-
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
